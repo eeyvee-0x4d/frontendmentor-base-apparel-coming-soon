088cbd30464fdcfd609124aa7513e5759723d12a
Al Vincent Musa <alvincentvibar.musa@bicol-u.edu.ph>
2023-02-07 18:21:52 +0800

    done

---
+++ index.js
@@ -0,0 +1,32 @@
+window.onload = (e) => {
+    document.querySelector(".email-form").reset()
+}
+
+const handleValueChange = () => {
+    let input_data = document.querySelector(".email-input").value
+    let email_regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+    let error_icon = document.querySelector(".error")
+    let error_message = document.querySelector(".error-message")
+
+
+    if(input_data !== "") {
+        if(input_data.match(email_regex) === null) {
+            error_icon.classList.add("error-active")
+            error_message.classList.add("error-active")
+        }
+        else if(input_data.value === "") {
+            error_icon.classList.remove("error-active")
+            error_message.classList.remove("error-active")
+        }
+        else {
+            error_icon.classList.remove("error-active")
+            error_message.classList.remove("error-active")
+        }
+    }
+    else {
+        error_icon.classList.remove("error-active")
+        error_message.classList.remove("error-active")
+    }
+    
+}
